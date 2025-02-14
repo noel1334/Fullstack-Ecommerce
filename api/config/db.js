e9688ee60b34e492a2636a24176fbd474edb6b6d
noel334 <noelvictor1334@gmail.com>
2025-02-14 13:49:15 +0100

    update my env

--- api/config/db.js
+++ api/config/db.js
@@ -6,11 +6,12 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    
     console.log('MongoDB connected...');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
-  }
+  } 
 };
 
 export default connectDB;
