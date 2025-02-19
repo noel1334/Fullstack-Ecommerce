f20d72595685037fe11a0ea334321d77939c1c74
noel334 <noelvictor1334@gmail.com>
2025-02-19 07:47:14 +0100

    changes for deployment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: "https://mystore-livid-omega.vercel.app/api/",
   headers: {
     "Content-Type": "multipart/form-data", 
   },
